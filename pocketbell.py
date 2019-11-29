f3f3cdbb13d79323d3683072da86b80edc724e22
yoneyan <github.yoneyan.net>
2019-11-29 17:30:41 +0900

    add dictionary

--- pocketbell.py
+++ pocketbell.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import sys
-#import threading
+import threading
 #from flask import Flask
+import time
+
 from asterisk.agi import *
 
 dial_first = []
@@ -14,6 +16,12 @@ def dial_input():
     agi.verbose(x)
     return x
 
+"""
+def judgement():
+    while True:
+        if len(dial) % 2 == 0:
+            send.append(dial.pop(0))
+"""
 
 agi = AGI()
 
@@ -21,22 +29,35 @@ agi.verbose("Started")
 callerId = agi.env['agi_callerid']
 agi.verbose("Call from %s" % callerId)
 
+'''
 while len(dial_first) != 4:
     result = dial_input()
     dial_first.append(result)
-    agi.say_number(result)
-    agi.verbose(dial_first)
+    agi.say_ntime.sleepumber(result)
+    agi.verbose(dial_first)# if len(dial) != 0:
+'''
 
-    #    threading.Thread(target=judgment()).start()
+#threading.Thread(target=judgement).start()
 
 while True:
     result = dial_input()
     agi.verbose("got digit %s" % result)
     # if len(dial) != 0:
     dial.append(result)
-    agi.verbose(dial)
+    agi.verbose("dial: " + ("".join(dial)))
+    agi.verbose("send: " + ("".join(send)))
     if result.isdigit():
         agi.say_number(result)
+    if len(dial) % 2 == 0:
+        text_first = dial[-2]
+        text_second = dial[-1]
+        agi.verbose("dial: " + ("".join(dial)))
+        dic = {11: "あ", 12: "い", 13: "う", 14: "え", 15: "お"}
+
+
+
+
+
 
 """
     elif result[-2] == "#" and result[-1] == "#":
